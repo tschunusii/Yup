d4a7593a62f2ea673f6340f569670928eff08088
tschunusii <tschunusii@gmail.com>
2024-11-12 22:03:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 const fetch = require('node-fetch'); // Falls node-fetch verwendet wird
 
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
-// CORS-Proxy-Route für CoinGecko
-app.get('/api/memecoins', async (req, res) => {
+// Proxy-Route zur Umgehung des CORS-Problems für spezifische Coin-Daten
+app.get('/api/coin/:coinId', async (req, res) => {
+    const coinId = req.params.coinId;
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinId}`;
+
     try {
-        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=meme-token&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=1h,24h,7d');
+        const response = await fetch(url);
         const data = await response.json();
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(data);
     } catch (error) {
-        console.error("Fehler beim Abrufen der CoinGecko-Daten:", error);
-        res.status(500).send('Fehler beim Abrufen der Daten');
+        console.error("Fehler beim Abrufen der Coin-Daten:", error);
+        res.status(500).send('Fehler beim Abrufen der Coin-Daten');
     }
 });
 
